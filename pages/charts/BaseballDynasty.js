e2b6dcaa3e2994db8e865dede0f19ae2bf45c047
unknown <willmcgrath96@gmail.com>
2022-05-05 13:35:29 -0500

    fixed closeorigin

--- pages/charts/BaseballDynasty.js
+++ pages/charts/BaseballDynasty.js
@@ -26,7 +26,7 @@ const BaseballDynasty = (props) => {
       <Head>
         <title>Beelzebufo</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&display=swap"
           rel="stylesheet"
--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home(props) {
       <Head>
         <title>Beelzebufo</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&display=swap"
           rel="stylesheet"
